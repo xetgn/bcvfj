e40b3b042d0b95c1a8485fcab8f082d581e1d7dd
xetgn <118056714+xetgn@users.noreply.github.com>
2023-08-12 10:07:47 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'08233fbd-e411-44a5-92a5-fcec2ed236c7'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/08233fbd-e411-44a5-92a5-fcec2ed236c7-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/08233fbd-e411-44a5-92a5-fcec2ed236c7-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/08233fbd-e411-44a5-92a5-fcec2ed236c7-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/08233fbd-e411-44a5-92a5-fcec2ed236c7-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
